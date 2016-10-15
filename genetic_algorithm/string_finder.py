576fd8660eb04fd9a904750c453d6c55d6680a36
Igor Tereschenko <pokawork@gmail.com>
2016-10-15 05:57:21 +0300

    it works

--- genetic_algorithm/string_finder.py
+++ genetic_algorithm/string_finder.py
@@ -6,10 +6,20 @@ class Genetic(object):
     def __init__(self, target_string, offsprings_number, required_fitness):
         self.target_string = target_string
         self.generations = 0
-        self.initial_population = generate_string(len(self.target_string))
+        self.initial_population = self.generate_string(len(self.target_string))
         self.offsprings = []
         self.offsprings_number = offsprings_number
         self.required_fitness = required_fitness
+
+    def generate_string(self, length):
+        return "".join(random.choice(string.lowercase) for i in xrange(length))
+
+    def fitness(self, e_string):
+        fitness = 0
+        for i in xrange(len(self.target_string)):
+            if self.target_string[i] == e_string[i]:
+                fitness += 1
+        return (100 / float(len(self.target_string))) * fitness
 
     def mutate(self, estring):
         """change random letter to a random value"""
@@ -18,44 +28,28 @@ class Genetic(object):
         return "".join(estring)
 
     def fill_offsprings(self, string):
+        """fill self.offsprings with changed by 1 char strings"""
         for i in xrange(self.offsprings_number):
             self.offsprings.append(self.mutate(string))
 
     def evolve(self, olist):
-        """remove elements not fit enough"""
-        offsprings = filter(lambda i: fitness(i, self.target_string) >= self.required_fitness,
-                            olist)
-        return offsprings
+        """returns the most fit element"""
+        return max(olist, key=lambda x: self.fitness(x))
 
     def evolution(self):
-        self.fill_offsprings(self.initial_population)
-        survivors = self.evolve(self.offsprings)
-        while(self.target_string not in self.offsprings):
-            self.offsprings = []
+        current_candidate = self.initial_population
+        while current_candidate != self.target_string:
+            self.fill_offsprings(current_candidate)
+            current_candidate = self.evolve(self.offsprings)
             self.generations += 1
-            for i in survivors:
-                self.fill_offsprings(i)
-            survivors = self.evolve(self.offsprings)
-            print survivors
+            print "{}: {}".format(self.generations, current_candidate)
         return self.generations
-
-
-def fitness(s1, s2):
-    fitness = 0
-    for i in xrange(len(s1)):
-        if s1[i] == s2[i]:
-            fitness += 1
-    return (100 / float(len(s1))) * fitness
-
-
-def generate_string(length):
-    return "".join(random.choice(string.lowercase) for i in xrange(length))
 
 
 def main():
     sample_string = "lpsykongru"
     g = Genetic(sample_string, 10, 10.0)
-    print g.evolution()
+    g.evolution()
 
 if __name__ == "__main__":
     main()
